46e2d798581669c5f03af3533e2ba00a8d72ddea
Yu-Fu Fu <a0919610611@gmail.com>
2020-12-02 05:15:58 -0500

    fix slow paste

--- zsh-common/zsh-common.zsh
+++ zsh-common/zsh-common.zsh
@@ -235,3 +235,16 @@ if (( $+commands[keychain] ));then
     [[ -f $HOME/.keychain/$HOSTNAME-sh ]] && source $HOME/.keychain/$HOSTNAME-sh
     [[ -f $HOME/.ssh/id_ed25519 ]] && keychain $HOME/.ssh/id_ed25519 2>/dev/null
 fi
+
+### Fix slowness of pastes with zsh-syntax-highlighting.zsh
+pasteinit() {
+  OLD_SELF_INSERT=${${(s.:.)widgets[self-insert]}[2,3]}
+  zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
+}
+
+pastefinish() {
+  zle -N self-insert $OLD_SELF_INSERT
+}
+zstyle :bracketed-paste-magic paste-init pasteinit
+zstyle :bracketed-paste-magic paste-finish pastefinish
+### Fix slowness of pastes
