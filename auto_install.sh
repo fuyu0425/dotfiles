da8e7c4cf0b41d9ef6bac56c56a5114f91177228
Yu-Fu Fu <a0919610611@gmail.com>
2020-12-30 23:31:40 -0500

    better

--- auto_install.sh
+++ auto_install.sh
@@ -21,27 +21,12 @@ git submodule update --recursive
 here=$PWD
 echo "the dotfile location is $here"
 cat /etc/shells | grep -q "zsh"
-have_zsh=$?
-
 mkdir -p $HOME/bin
-
-
-echo "install YouCompletme? (y/n) default is y"
-read answer
-install_youcompletme=1
-if echo "${answer:=y}"|grep -iq '^y' ;then
-    install_youcompletme=1
-else
-    install_youcompletme=0
-fi
 
 # Darwin is MacOS
 if [ $OS = "Darwin" ] ;then
     $STOW zsh-mac
 elif [ $OS = "Linux" ] ;then
-    if [ $have_zsh -ne 0 ];then
-        sudo apt install -y zsh
-    fi
     $STOW zsh-linux
 fi
 
@@ -52,18 +37,6 @@ $STOW vim
 $STOW git
 $STOW editorconfig
 
-if [ $install_youcompletme -eq 0 ];then
-    sed -i -e s@"Plug 'Valloric/YouCompleteMe'"@"\"Plug 'Valloric/YouCompleteMe'"@g $HOME/.vimrc
-fi
-
 vim +PlugInstall +qall
-
-if [ $install_youcompletme -ne 0 ];then
-    echo "build youcomplete me yourself"
-fi
-echo "complete"
-
-# git config
-git config --global credential.helper cache
 
 exec zsh
