51ed654d70db74f1500a03e94a2263da29cfc8c9
Yu-Fu Fu <yufu@yfu.tw>
2023-03-09 22:09:24 -0500

    add arch install script

---
+++ auto_install_arch.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -v
+
+SCRIPT_DIR=$(dirname $(realpath $0))
+export PATH=$HOME/.local/bin:$PATH
+#$SCRIPT_DIR/scripts/install_tools
+hash -r
+
+if ! command -v stow &> /dev/null
+then
+    echo "stow could not be found, installing"
+    $SCRIPT_DIR/scripts/install_stow
+    hash -r
+fi
+
+STOW="stow --dotfiles"
+OS=$(uname -s)
+echo "OS is $OS"
+echo "start dotfile install"
+git submodule init
+git submodule update --recursive
+here=$PWD
+echo "the dotfile location is $here"
+cat /etc/shells | grep -q "zsh"
+mkdir -p $HOME/bin
+
+cp $SCRIPT_DIR/misc/bin/* $HOME/bin
+
+# Darwin is MacOS
+if [ $OS = "Darwin" ] ;then
+    $STOW zsh-mac
+elif [ $OS = "Linux" ] ;then
+    $STOW zsh-linux
+fi
+
+LOCAL_ZSHENV=$HOME/.zshenv.local
+touch $LOCAL_ZSHENV
+
+$STOW zsh-common
+
+$STOW tmux
+$STOW vim
+$STOW git
+$STOW editorconfig
+
+VIM=vim
+if command -v nvim &> /dev/null; then
+    VIM=nvim
+fi
+
+$VIM +PlugInstall +qall
+
+
+# htop
+# cp $SCRIPT_DIR/htop/htoprc ~/.config/htop
+
+exec zsh
